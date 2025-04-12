bf20918bf126ac7e0d8075329f4a200fdf36dc94
Ahmed-Nakhuda <144385153+Ahmed-Nakhuda@users.noreply.github.com>
2025-04-12 17:25:34 -0400

    Update cors

--- vite-project/src/server.js
+++ vite-project/src/server.js
@@ -14,10 +14,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
+  origin: 'https://resilient-hearts-qqz4glnlf-ahmed-nakhudas-projects.vercel.app,
+  credentials: true
 }));
 
 
